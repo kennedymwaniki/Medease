188a0c2665d518636e1ce0c8e7259c7b6cf92d4f
kennedymwaniki <kennedymwaniki089@gmail.com>
2025-07-29 09:30:27 +0300

    footer fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,17 +1,6 @@
 // import addNotification from 'react-push-notification'
 
 const Footer = () => {
-  // const buttonClick = () => {
-  //   console.log('Clicked')
-  //   addNotification({
-  //     title: 'Warning',
-  //     // position: 'bottom-right',
-  //     subtitle: 'This is a subtitle',
-  //     message: 'This is a very long message',
-  //     theme: 'darkblue',
-  //     native: true, // when using native, your OS will handle theming.
-  //   })
-  // }
   return (
     <footer className="bg-black text-white rounded-3xl mx-auto mb-2 mr-3 ml-3">
       {/* CTA Section */}
